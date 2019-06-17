b5ee7d9dc9407cee3d6f1f7993093f42917a98f1
Maksim Grishkin <FFFFF@bk.ru>
2019-06-17 20:08:36 +0300

    Рефакторинг. Реструктурированы ТЗ на геопортал и кад. учет

--- technicalTasks/aerialPhoto/adoc2doxc.bat
+++ technicalTasks/aerialPhoto/adoc2doxc.bat
@@ -1,6 +1,34 @@
-set file=_technicalTask.adoc
+Setlocal EnableDelayedExpansion
+@echo off
+rem Разрешить изменение переменных в цикле
+chcp 65001
+echo =======================================================================
+echo Автоматически конвертирует документ asciidoctor в формат docx
+echo На компьютере должен стоять asciidoctorj и pandoc
+echo Название конвертируемого файла должно начинаться на подчеркивание (_)
+echo =======================================================================
+echo.
+rem for %%i in (_*.adoc) do (    
+rem for /f "delims=" %%i in (`dir /b _*.adoc`) do (
+for %%i in (_*.adoc) do (  
+    set file=%%i
+    set newfile=%%~ni%.docx
+    set newfile=!newfile:~1!
+    set tempfile=%%i%.temp.xml
 
-asciidoctorj --backend docbook --out-file 1.xml %file%
-pandoc --from docbook --to docx --output %file:~0,-5%.docx 1.xml
-del 1.xml
+    echo. 
+    echo Обнаружен файл "!file!"
+    echo.
+    echo Конвертируем adoc во временный файл в формате Docbook с помощью asciidoctorj
+    echo.
+    asciidoctorj --backend docbook --out-file "!tempfile!" "!file!"
+    echo.
+    echo Коневертируем временный Docbook в формат docx с помощью pandoc
+    echo.
+    pandoc --from docbook --to docx --output "!newfile!":~1 "!tempfile!"
+    echo Очищаем мусор
+    del "!tempfile!"
+    echo.
+)
+
 pause
