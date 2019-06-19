2f0de0de9ca129fe6ed6fa43c28ea35f73c04d15
Maksim Grishkin <FFFFF@bk.ru>
2019-06-19 12:00:31 +0300

    Скрипт трансформации в docx обходит все каталоги

--- out/adoc2doxc.bat
+++ out/adoc2doxc.bat
@@ -1,6 +1,8 @@
-Setlocal EnableDelayedExpansion
 @echo off
 rem Разрешить изменение переменных в цикле
+Setlocal EnableDelayedExpansion
+
+rem Установить кодировку utf-8
 chcp 65001
 echo =======================================================================
 echo Автоматически конвертирует документ asciidoctor в формат docx
@@ -8,24 +10,26 @@ echo На компьютере должен стоять asciidoctorj и pandoc
 echo Название конвертируемого файла должно начинаться на подчеркивание (_)
 echo =======================================================================
 echo.
-rem for %%i in (_*.adoc) do (    
-rem for /f "delims=" %%i in (`dir /b _*.adoc`) do (
-for %%i in (_*.adoc) do (  
+for /R .\..\ %%i in (_*.adoc) do (  
     set file=%%i
     set newfile=%%~ni%.docx
     set newfile=!newfile:~1!
-    set tempfile=%%i%.temp.xml
+    set tempfile=%%~ni%.temp.xml
+
+    echo !file!
+    echo !newfile!
+    echo !tempfile!
 
     echo. 
     echo Обнаружен файл "!file!"
     echo.
     echo Конвертируем adoc во временный файл в формате Docbook с помощью asciidoctorj
     echo.
-    asciidoctorj --backend docbook --out-file "!tempfile!" "!file!"
+    asciidoctorj --backend docbook --out-file "!tempfile!" "!file!" --section-numbers
     echo.
     echo Коневертируем временный Docbook в формат docx с помощью pandoc
     echo.
-    pandoc --from docbook --to docx --output "!newfile!":~1 "!tempfile!"
+    pandoc --from docbook --to docx --output "!newfile!" "!tempfile!" --number-sections
     echo Очищаем мусор
     del "!tempfile!"
     echo.
